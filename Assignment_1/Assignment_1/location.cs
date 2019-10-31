46b0e569dba6a60e2d0d411bb392ca70bb70dd10
CharlotteHolley <56397960+CharlotteHolley@users.noreply.github.com>
2019-10-31 17:49:52 +0000

    Updated shapes
    updated shape controls, added the drawto function and reset button working

---
+++ Assignment_1/Assignment_1/location.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class location
+    {
+        private int _x;
+        private int _y;
+
+        public int x
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public int y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+    }
+    
+}
