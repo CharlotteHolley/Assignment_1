e7964ba2c6921de3688d0e08d91165931e2cef3f
CharlotteHolley <56397960+CharlotteHolley@users.noreply.github.com>
2019-10-31 19:15:27 +0000

    Final commit of 31/10
    Few more commands added (clear function added to textarea)

--- Assignment_1/Assignment_1/drawShape.cs
+++ Assignment_1/Assignment_1/drawShape.cs
@@ -32,19 +32,19 @@ namespace Assignment_1
         public Graphics drawTriangle(string[] scaleCmds, Graphics g)
         {
 
-            Point[] pnt = new Point[3];
+            Point[] TriPoints = new Point[3];
 
-            pnt[0].X = Convert.ToInt16(scaleCmds[1].Trim()); //Example size: 150
-            pnt[0].Y = Convert.ToInt16(scaleCmds[2].Trim()); //Example size: 150
+            TriPoints[0].X = Convert.ToInt16(scaleCmds[1].Trim()); //Example size: 150
+            TriPoints[0].Y = Convert.ToInt16(scaleCmds[2].Trim()); //Example size: 150
 
-            pnt[1].X = Convert.ToInt16(scaleCmds[3].Trim()); //Example size: 150
-            pnt[1].Y = Convert.ToInt16(scaleCmds[4].Trim()); //Example size: 200
+            TriPoints[1].X = Convert.ToInt16(scaleCmds[3].Trim()); //Example size: 150
+            TriPoints[1].Y = Convert.ToInt16(scaleCmds[4].Trim()); //Example size: 200
 
-            pnt[2].X = Convert.ToInt16(scaleCmds[5].Trim()); //Example size: 50
-            pnt[2].Y = Convert.ToInt16(scaleCmds[6].Trim()); //Example size: 120
+            TriPoints[2].X = Convert.ToInt16(scaleCmds[5].Trim()); //Example size: 50
+            TriPoints[2].Y = Convert.ToInt16(scaleCmds[6].Trim()); //Example size: 120
 
             Pen pen = new Pen(Color.Black, 2);
-            g.DrawPolygon(pen, pnt);
+            g.DrawPolygon(pen, TriPoints);
             
             return g;
         }
